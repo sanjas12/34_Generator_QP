f8d08d790448383c613ca9db694ded3eb71d6acc
sanja_s <sanja100v@gmail.com>
2025-08-14 22:30:33 +0300

    `Updated README and setup.py for Generator QP project`

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import sys
 # import os
 from cx_Freeze import setup, Executable
 
-# Путь к директории Documentation
-documentation_path = os.path.join(os.path.dirname(__file__), 'Documentation')
+# Путь к директории Documentation внутри проекта
+documentation_path = os.path.join(os.path.dirname(__file__), 'generator_qp', 'src', 'Documentation')
 
 file = "setup.py"
 name = "Generator QP"
@@ -27,7 +27,7 @@ with open(file, 'r+', encoding='utf-8') as f:
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    "path": sys.path + ["src"],  # Добавляем путь к 'src'
+    "path": sys.path + [os.path.join(os.path.dirname(__file__), "generator_qp", "src")],  # Добавляем путь к фактической 'src'
     "excludes": ["tkinter", "unittest", "http",
                 "PyQT5.QtopenGL4",
                 "pydoc_data", "email",
@@ -35,10 +35,10 @@ build_exe_options = {
                 # "xml",
                 # "asyncio", "curses", "distutils", "html", "multiprocessing",
                 "sqlite3", "test", "urlib"],
-    "optimize": 0,      # c 2 exe не запускается
+    "optimize": 1,      # c 2 exe не запускается
     # "zip_include_packages": ["PyQt5", "matplotlib"],
     # "include_files" : files
-    "include_files": [(documentation_path, 'Documentation/'), ('src/config/config.py', 'config/config.py')]
+    "include_files": [(documentation_path, 'Documentation/')]
 }
 
 # base="Win32GUI" should be used only for Windows GUI app. If comment this line, will appear console
@@ -50,5 +50,5 @@ setup(
     description="My Generator QP",
     options={"build_exe": build_exe_options},
     # executables=[Executable("main.py", target_name=name,  base=base)],
-    executables=[Executable("src/main.py", target_name=name)],
+    executables=[Executable("generator_qp/src/main.py", target_name=name)],
 )
